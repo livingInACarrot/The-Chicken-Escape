3d899ef82a9021680df834363ca5ff4cb00d6d7d
Tatyana Guzovskaya <tkguzovskaya@edu.hse.ru>
2024-03-26 02:27:11 +0300

    bro, this kinda...

---
+++ The Chicken Escape 2.1/Assets/Scripts/Objects Behaviour/ChickenInteractions.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class ChickenInteractions : MonoBehaviour
+{
+    public Animator chickenAnimator;
+    public Button eatButton; // Assign this in the inspector
+    public Button drinkButton; // Assign this in the inspector
+    private bool isFeeding = false;
+
+    private void Start()
+    {
+        chickenAnimator = GetComponent<Animator>();
+        // Hide the buttons initially
+        eatButton.gameObject.SetActive(false);
+        drinkButton.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (!isFeeding)
+        {
+            if (other.CompareTag("Eat"))
+            {
+                // Show the eating button.
+                eatButton.gameObject.SetActive(true);
+                eatButton.onClick.AddListener(delegate() {
+                    eatButton.GetComponentInChildren<TMP_Text>().text = "stop";
+                    StartCoroutine(PlayEatAnimation()); 
+                });
+
+            }
+            // Check for the water object by tag.
+            else if (other.CompareTag("Drink"))
+            {
+                // Show the drinking button.
+                drinkButton.gameObject.SetActive(true);
+                drinkButton.onClick.AddListener(delegate () {
+                    drinkButton.GetComponentInChildren<TMP_Text>().text = "stop";
+                    StartCoroutine(PlayDrinkAnimation());
+                });
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        eatButton.gameObject.SetActive(false);
+        drinkButton.gameObject.SetActive(false);
+        Stop();
+    }
+    private void Stop()
+    {
+        isFeeding = false;
+        eatButton.GetComponentInChildren<TMP_Text>().text = "eat";
+        drinkButton.GetComponentInChildren<TMP_Text>().text = "drink";
+        chickenAnimator.Play("chicken_run");
+    }
+
+    IEnumerator PlayEatAnimation()
+    {
+        isFeeding = true;
+        chickenAnimator.Play("chicken_eat");
+        eatButton.onClick.AddListener(() => Stop());
+        yield return new WaitForSeconds(4);
+    }
+
+    IEnumerator PlayDrinkAnimation()
+    {
+        isFeeding = true;
+        chickenAnimator.Play("chicken_drink");
+        drinkButton.onClick.AddListener(() => Stop());
+        yield return new WaitForSeconds(5);
+    }
+}
