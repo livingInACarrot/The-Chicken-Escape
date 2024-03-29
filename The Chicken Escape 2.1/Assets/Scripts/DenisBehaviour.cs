49a12e47257e9033a46badc22283846110fedfda
Tatyana Guzovskaya <tkguzovskaya@edu.hse.ru>
2024-03-29 18:26:33 +0300

    his first steps... so touching!

--- The Chicken Escape 2.1/Assets/Scripts/DenisBehaviour.cs
+++ The Chicken Escape 2.1/Assets/Scripts/DenisBehaviour.cs
@@ -7,21 +7,36 @@ public class DenisBehaviour : MonoBehaviour
     private Animator animator;
     private int hours;
     private int minutes;
+    public float speed = 3f;
+    private float range = 0.1f;
+    private bool block = true;
 
     // Points to visit on map
-    public GameObject home;
-    public GameObject barn;
-    public GameObject pit;
-    public GameObject point1;
-    public GameObject point2;
-    public GameObject bottomGarden;
-    public GameObject hutFence;
-    public GameObject hutEntrance;
-    public GameObject topRightBed;
+    public Transform home;
+    public Transform nearHome;
+    public Transform betweenHomeAndBarn;
+    public Transform leftFromBarn;
+    public Transform nearBarn;
+    public Transform barn;
+    public Transform pit;
+    public Transform point1;
+    public Transform point2;
+    public Transform bottomGarden;
+    public Transform hutFence;
+    public Transform hutEntrance;
+    public Transform topRightBed;
+
+    private int currentDestination = 0;
+    private List<Transform> routeToBarn;
+    private Vector2 way;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        HideAllPoints();
+        routeToBarn = new () { home, nearHome, betweenHomeAndBarn, leftFromBarn, nearBarn, barn};
+        way = routeToBarn[0].position;
+        MirrorAnimation();
     }
 
     void Update()
@@ -29,25 +44,70 @@ public class DenisBehaviour : MonoBehaviour
         hours = TimerClock.Hours();
         minutes = TimerClock.Minutes();
 
-        if (hours == 11 && minutes == 30)
-        {
+        GoToBarn();
 
+        if (!block)
+        {
+            if (hours == 11 && minutes == 30)
+            {
+                GoToBarn();
+            }
+            if (hours == 13 && minutes == 0)
+            {
+                //StartCoroutine(GoToBarn());
+                //GoToBarn();
+            }
+            if (hours == 15 && minutes == 0)
+            {
+
+            }
+            if (hours == 17 && minutes == 0)
+            {
+
+            }
+            if (hours == 20 && minutes == 0)
+            {
+
+            }
         }
-        if (hours == 13 && minutes == 0)
-        {
 
-        }
-        if (hours == 15 && minutes == 0)
-        {
+    }
+    private void HideAllPoints()
+    {
+        //home.gameObject.SetActive(false);
+        // ect.
+    }
+    private void MirrorAnimation()
+    {
+        if (way.x < transform.position.x)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else if (way.x > transform.position.x)
+            transform.localScale = new Vector3(1, 1, 1);
 
-        }
-        if (hours == 17 && minutes == 0)
+    }
+    private string ChooseAnimation()
+    {
+        if (Mathf.Abs(way.x - transform.position.x) < Mathf.Abs(way.y - transform.position.y) &&
+            transform.position.y < way.y)
+            return "back_walk";
+        else if (Mathf.Abs(way.x - transform.position.x) < Mathf.Abs(way.y - transform.position.y) &&
+            transform.position.y >= way.y)
+            return "towards_walk";
+        else
+            return "side_walk";
+    }
+    void GoToBarn()
+    {
+        //block = true;
+        animator.Play(ChooseAnimation());
+        transform.position = Vector2.MoveTowards(transform.position, way, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, way) < range)
         {
-
-        }
-        if (hours == 20 && minutes == 0)
-        {
-
+            currentDestination += 1;
+            if (currentDestination >= routeToBarn.Count)
+                return;
+            way = routeToBarn[currentDestination].position;
+            MirrorAnimation();
         }
     }
 }
